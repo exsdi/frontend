e76cc0dd9ccb9a85c702a70de6e94160539e8a78
exsdi <sandiegoexclusive@gmail.com>
2023-10-02 19:06:19 -0700

    removed /:

--- src/components/WorkoutDetails.js
+++ src/components/WorkoutDetails.js
@@ -4,7 +4,7 @@ const WorkoutDetails = ({ workout }) => {
   const { dispatch } = useWorkoutsContext();
 
   const handleDelete = async () => {
-    const response = await fetch(`https://backend-api-duz9.onrender.com/${workout._id}`, {
+    const response = await fetch(`https://backend-api-duz9.onrender.com${workout._id}`, {
       method: "DELETE",
     });
 
